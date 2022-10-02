7c9f17d66ef44e4bed56ab634d8e71903680c6b9
陈开邦 <912453237@qq.com>
2022-10-02 20:55:03 +0800

    feature: - 脚本更新

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,6 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:YunShangZhou/learn-javascript.git master:gh-pages
+git push -f git@github.com:YunShangZhou/EbbinghausTable.git master:gh-pages
 
 cd -
